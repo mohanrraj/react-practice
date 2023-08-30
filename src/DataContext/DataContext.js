0dfd2f8c611eba6b3ad5fdde758f8fa43e03d6ec
mohanrraj <mohanrraj1994@gmail.com>
2023-08-30 20:03:36 +0530

    Using regular fetch instead of axios

--- src/DataContext/DataContext.js
+++ src/DataContext/DataContext.js
@@ -6,6 +6,7 @@ import {format} from "date-fns";
 const DataContext = createContext({});
 
 export const DataProvider = ({children}) => {
+    const URL = "http://localhost:3500/items";
 
     const [items, setItems] = useState([]);
     const [searchedItems, setSearchedItems] = useState([]);
@@ -18,8 +19,9 @@ export const DataProvider = ({children}) => {
 
         const fetchData = async () => {
             try{
-                const response = await api.get("/items");
-                setItems([...response.data]);
+                const response = await fetch(URL);
+                const data = await response.json();
+                setItems([...data]);
             }
             catch(err){
                 console.log(err.message);
@@ -53,8 +55,17 @@ export const DataProvider = ({children}) => {
         const currentTime = format(new Date(),"yyyy dd, MMMM pp");
         console.log(currentTime);
         const newItem = {id, datetime : currentTime, task};
+
+        const optionsObj = {
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(newItem)
+        }
+
         try{
-            const response = await api.post("/items", newItem);
+            const response = await fetch(URL, optionsObj);
             navigate("/");
         }catch(err){
             console.log(err.message);
@@ -64,7 +75,7 @@ export const DataProvider = ({children}) => {
     const handleDelete = async (id) => {
         // console.log(id)
         try{
-            await api.delete(`/items/${id}`);
+            await fetch(`${URL}/${id}`, {method : "DELETE"});
             navigate("/");
         }catch(err){
             console.log(err.message)
@@ -77,8 +88,16 @@ export const DataProvider = ({children}) => {
 
         items.map(item => item.id === id ? {...newItem} : item);
 
+        const optionObj = {
+            method : "PUT",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(newItem)
+        }
+
         try{
-            const response = api.put(`/items/${id}`, newItem);
+            const response = fetch(`${URL}/${id}`, optionObj);
             navigate("/");
         }catch(err){
             console.log(err.message);
